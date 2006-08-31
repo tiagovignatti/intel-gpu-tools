863cd6a5b757740fdfb0598dbfdc4ee9df4be050
Eric Anholt <eric@anholt.net>
2006-08-31 14:46:28 -0700

    Add more unary operation tests.

--- assembler/test/run-test.sh
+++ assembler/test/run-test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-TESTDIR=${srcdir-`pwd`}
+SRCDIR=${srcdir-`pwd`}
 BUILDDIR=${top_builddir-`pwd`}
 
-${BUILDDIR}/src/gen4asm -o TEST.out $TESTDIR/TEST.g4a
-if cmp TEST.out ${TESTDIR}/TEST.expected; then : ; else
+${BUILDDIR}/src/gen4asm -o TEST.out $SRCDIR/TEST.g4a
+if cmp TEST.out ${SRCDIR}/TEST.expected 2> /dev/null; then : ; else
+  echo "Output comparison for TEST"
+  diff -u ${SRCDIR}/TEST.expected TEST.out
   exit 1;
 fi
