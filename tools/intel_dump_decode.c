aa4772c3c1c09f775e34e15533789094922f2e40
Chris Wilson <chris@chris-wilson.co.uk>
2010-07-15 19:31:21 +0100

    dump decode: Add a non-installed program for decoding binary batchbuffer dumps.
    Rather than add yet another decoder into the drivers, I rather just emit
    a binary blob and use the decoder being developed here.
    
    As it is a pure debugging feature, lets not package it.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

---
+++ tools/intel_dump_decode.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2010 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Chris Wilson <chris@chris-wilson.co.uk>
+ *
+ */
+
+#define _GNU_SOURCE
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "intel_decode.h"
+
+static void
+read_bin_file (uint32_t devid, const char * filename)
+{
+	uint32_t buf[2048];
+	int fd, offset, ret;
+
+	fd = open (filename, O_RDONLY);
+	if (fd < 0) {
+		fprintf (stderr, "Failed to open %s: %s\n",
+			 filename, strerror (errno));
+		exit (1);
+	}
+
+	offset = 0;
+	while ((ret = read (fd, buf, sizeof(buf))) > 0) {
+		intel_decode (buf, ret/4, offset, devid, 1);
+		offset += ret;
+	}
+	close (fd);
+}
+
+int
+main (int argc, char *argv[])
+{
+	uint32_t devid = 0xa011;
+	int i;
+
+	for (i = 1; i < argc; i++)
+		read_bin_file (devid, argv[i]);
+
+	return 0;
+}
