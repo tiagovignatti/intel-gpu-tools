61ff1309cabaa1fc879ed0c3179696ad64a79da3
Chris Wilson <chris@chris-wilson.co.uk>
2011-12-12 17:44:58 +0000

    scripts: Add a trivial script to show which programs are using the GPU
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

---
+++ scripts/who.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# usage: sudo who.sh
+#
+# Requires root permissions to both query who has the device open,
+# and to read the mappings of likely root-owned processes
+#
+
+for i in `lsof -t /dev/dri/card0`; do
+	who=`readlink /proc/$i/exe`
+	count=`grep /dev/dri/card0 /proc/$i/maps | wc -l | cut -f1 -d\ `
+	echo "$who: $count"
+done
