6cfd16648d8d229c4ab474e4beff1cc131287238
Kenneth Graunke <kenneth@whitecape.org>
2012-02-10 22:19:16 -0800

    Remove pointless Android ifdefs for sys/fcntl.h.
    On my system, sys/fcntl.h contains exactly one line:
    
       #include <fcntl.h>
    
    So there's really no need to #ifdef it.  Also, intel_mmio.c already
    included <fcntl.h>; there's no need to include it twice.
    
    Signed-off-by: Kenneth Graunke <kenneth@whitecape.org>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- lib/intel_drm.c
+++ lib/intel_drm.c
@@ -36,11 +36,7 @@
 #include <err.h>
 #include <assert.h>
 #include <sys/ioctl.h>
-#ifdef ANDROID
 #include <fcntl.h>
-#else
-#include <sys/fcntl.h>
-#endif
 #include <sys/stat.h>
 #include <sys/mman.h>
 #ifdef HAVE_STRUCT_SYSINFO_TOTALRAM
--- lib/intel_mmio.c
+++ lib/intel_mmio.c
@@ -38,11 +38,6 @@
 #include <err.h>
 #include <assert.h>
 #include <sys/ioctl.h>
-#ifdef ANDROID
-#include <fcntl.h>
-#else
-#include <sys/fcntl.h>
-#endif
 #include <sys/stat.h>
 #include <sys/mman.h>
 
