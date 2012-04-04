888df75a0cc3e6eea485ae3189afde2d86486750
Ben Widawsky <ben@bwidawsk.net>
2012-04-03 23:02:58 -0700

    tests: use common code for scripts
    This removes cut and pasted code and uses a more central source.
    
    Signed-off-by: Ben Widawsky <benjamin.widawsky@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

---
+++ tests/drm_lib.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+die() {
+	echo "$@"
+	exit 1
+}
+
+if [ -d /debug/dri ] ; then
+	debugfs_path=/debug_dri
+fi
+
+if [ -d /sys/kernel/debug/dri ] ; then
+	debugfs_path=/sys/kernel/debug/dri
+fi
+
+i915_path=x
+for dir in `ls $debugfs_path` ; do
+	if [ -f $debugfs_path/$dir/i915_error_state ] ; then
+		i915_path=$debugfs_path/$dir
+		break
+	fi
+done
+
+if [ $i915_path = "x" ] ; then
+	die " i915 debugfs path not found."
+fi
+
+# read everything we can
+if [ `cat $i915_path/clients | wc -l` -gt "2" ] ; then
+	die "ERROR: other drm clients running"
+fi
+
+
