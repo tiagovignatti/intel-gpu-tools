3743a0b994a4e6035d9370ec658b70e0a8528c4a
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2013-06-27 16:54:42 +0300

    tests: rename debugfs base path to be more specific
    In preparation to have sysfs entries used in scripts
    rename to more specific name.
    
    Signed-off-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- tests/drm_lib.sh
+++ tests/drm_lib.sh
@@ -12,20 +12,20 @@ if [ -d /sys/kernel/debug/dri ] ; then
 	debugfs_path=/sys/kernel/debug/dri
 fi
 
-i915_path=x
+i915_dfs_path=x
 for dir in `ls $debugfs_path` ; do
 	if [ -f $debugfs_path/$dir/i915_error_state ] ; then
-		i915_path=$debugfs_path/$dir
+		i915_dfs_path=$debugfs_path/$dir
 		break
 	fi
 done
 
-if [ $i915_path = "x" ] ; then
+if [ $i915_dfs_path = "x" ] ; then
 	die " i915 debugfs path not found."
 fi
 
 # read everything we can
-if [ `cat $i915_path/clients | wc -l` -gt "2" ] ; then
+if [ `cat $i915_dfs_path/clients | wc -l` -gt "2" ] ; then
 	die "ERROR: other drm clients running"
 fi
 
