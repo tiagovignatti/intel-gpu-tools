4f8c9a2c386154a5be94e117b4371ab0ef8b55a8
Chris Wilson <chris@chris-wilson.co.uk>
2013-08-14 08:17:22 +0100

    gem_write_read_ring_switch: Copy the missing igt_require
    An oversight in the mass conversion to the new framework as the test was
    called from two locations.
    
    Ideally, the checks could be moved back to the caller and the framework
    still work. This is just the patch of least resistence.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=68091

--- tests/gem_write_read_ring_switch.c
+++ tests/gem_write_read_ring_switch.c
@@ -204,8 +204,13 @@ int main(int argc, char **argv)
 	for (i = 0; i < ARRAY_SIZE(tests); i++) {
 		char name[180];
 		snprintf(name, sizeof(name), "%s-interruptible", tests[i].name);
-		igt_subtest(name)
+		igt_subtest(name) {
+			gem_require_ring(fd, tests[i].ring);
+			/* Testing render only makes sense with separate blt. */
+			if (tests[i].ring == I915_EXEC_RENDER)
+				gem_require_ring(fd, I915_EXEC_BLT);
 			run_test(tests[i].ring, name);
+		}
 	}
 	igt_stop_signal_helper();
 
