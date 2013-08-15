92aca3b19e7201ed6e8f74b18207209d9312f8d2
Damien Lespiau <damien.lespiau@intel.com>
2013-08-15 16:40:47 +0100

    intel_infoframes: Dump 3D_Ext_Data when using Side-by-side (half)
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- tools/intel_infoframes.c
+++ tools/intel_infoframes.c
@@ -491,6 +491,11 @@ static void dump_vendor_hdmi(DipInfoFrame *frame)
 
 		printf("- 3D Format: %s\n",
 		       s3d_structure_to_string(s3d_structure));
+
+		/* Side-by-side (half) */
+		if (s3d_structure == 8)
+			printf("- 3D Ext Data 0x%x\n",
+			       frame->vendor.s3d_ext_data);
 	}
 }
 
