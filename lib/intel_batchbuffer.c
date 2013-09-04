tiagovignatti/intel-gpu-tools

commit bfbe813f8fb587017c4e1d73c51395c2837eb395

lib: fix gen5 workaround emission
Using BEGIN_BATCH can lead to a nice inf recursion through require_space
-> flush_batch -> BEGIN_BATCH.

Also fix things up to always require BATCH_RESERVED. We need 2 dwords
for the gen5 workaround and 2 dwords for MI_BB_END.

lib/intel_batchbuffer.c
@@ -84,10 +84,11 @@ intel_batchbuffer_flush_on_ring(struct intel_batchbuffer *batch, int ring)
 		return;
 
 	if (IS_GEN5(batch->devid)) {
-		BEGIN_BATCH(2);
-		OUT_BATCH(CMD_POLY_STIPPLE_OFFSET << 16);
-		OUT_BATCH(0);
-		ADVANCE_BATCH();
+		/* emit gen5 w/a without batch space checks - we reserve that
+		 * already. */
+		*(uint32_t *) (batch->ptr) = CMD_POLY_STIPPLE_OFFSET << 16;
+		*(uint32_t *) (batch->ptr) = 0;
+		batch->ptr += 8;
 	}
 
 	/* Round batchbuffer usage to 2 DWORDs. */

lib/intel_batchbuffer.h
@@ -63,7 +63,7 @@ static inline void
 intel_batchbuffer_require_space(struct intel_batchbuffer *batch,
                                 unsigned int sz)
 {
-	assert(sz < BATCH_SZ - 8);
+	assert(sz < BATCH_SZ - BATCH_RESERVED);
 	if (intel_batchbuffer_space(batch) < sz)
 		intel_batchbuffer_flush(batch);
 }

commit 4f496bafea81f24a3ae81179df7abaff648f0654

lib: fix the fix for gen5 workaround emmision
Fix the regression introduced in

commit bfbe813f8fb587017c4e1d73c51395c2837eb395
Author: Daniel Vetter <daniel.vetter@ffwll.ch>
Date:   Tue May 29 22:14:06 2012 +0200

    lib: fix gen5 workaround emission

Signed-off-by: Imre Deak <imre.deak@intel.com>

lib/intel_batchbuffer.c
@@ -87,8 +87,9 @@ flush_on_ring_common(struct intel_batchbuffer *batch, int ring)
 		/* emit gen5 w/a without batch space checks - we reserve that
 		 * already. */
 		*(uint32_t *) (batch->ptr) = CMD_POLY_STIPPLE_OFFSET << 16;
+		batch->ptr += 4;
 		*(uint32_t *) (batch->ptr) = 0;
-		batch->ptr += 8;
+		batch->ptr += 4;
 	}
 
 	/* Round batchbuffer usage to 2 DWORDs. */