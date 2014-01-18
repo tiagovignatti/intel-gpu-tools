724340cf36b4e2a87d8c545410224b617f813cc8
Ben Widawsky <benjamin.widawsky@intel.com>
2014-01-17 19:35:22 -0800

    quick_dump: Fix the indentation
    Fix the spaces to use [the python standard] 4 soft spaces for tabe.
    While here, add the proper vim tag so we don't do it again.
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>

--- tools/quick_dump/quick_dump.py
+++ tools/quick_dump/quick_dump.py
@@ -6,6 +6,8 @@
 # register types:
 #  '' - normal register
 #  'DPIO' - DPIO register
+#
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import argparse
 import os
@@ -16,17 +18,17 @@ import chipset
 import reg_access as reg
 
 def parse_file(file):
-	print('{0:^10s} | {1:^28s} | {2:^10s}'. format('offset', file.name, 'value'))
-	print('-' * 54)
-	for line in file:
-		register = ast.literal_eval(line)
-		if register[2] == 'DPIO':
-			val = reg.dpio_read(register[1], 0)
-		else:
-			val = reg.read(register[1])
-		intreg = int(register[1], 16)
-		print('{0:#010x} | {1:<28} | {2:#010x}'.format(intreg, register[0], val))
-	print('')
+    print('{0:^10s} | {1:^28s} | {2:^10s}'. format('offset', file.name, 'value'))
+    print('-' * 54)
+    for line in file:
+        register = ast.literal_eval(line)
+        if register[2] == 'DPIO':
+            val = reg.dpio_read(register[1], 0)
+        else:
+            val = reg.read(register[1])
+        intreg = int(register[1], 16)
+        print('{0:#010x} | {1:<28} | {2:#010x}'.format(intreg, register[0], val))
+    print('')
 
 
 parser = argparse.ArgumentParser(description='Dumb register dumper.')
@@ -36,39 +38,39 @@ parser.add_argument('profile', nargs='?', type=argparse.FileType('r'), default=N
 args = parser.parse_args()
 
 if reg.init() == False:
-	print("Register initialization failed")
-	sys.exit()
+    print("Register initialization failed")
+    sys.exit()
 
 # Put us where the script is
 os.chdir(os.path.dirname(sys.argv[0]))
 
 #parse anything named base_ these are assumed to apply for all gens.
 if args.baseless == False:
-	for root, dirs, files in os.walk('.'):
-		for name in files:
-			if name.startswith(("base_")):
-				file = open(name.rstrip(), 'r')
-				parse_file(file)
+    for root, dirs, files in os.walk('.'):
+        for name in files:
+            if name.startswith(("base_")):
+                file = open(name.rstrip(), 'r')
+                parse_file(file)
 
 if args.autodetect:
-	pci_dev = chipset.intel_get_pci_device()
-	devid = chipset.pcidev_to_devid(pci_dev)
-	if chipset.is_sandybridge(devid):
-		args.profile = open('sandybridge', 'r')
-	elif chipset.is_ivybridge(devid):
-		args.profile = open('ivybridge', 'r')
-	elif chipset.is_valleyview(devid):
-		args.profile = open('valleyview', 'r')
-	elif chipset.is_haswell(devid):
-		args.profile = open('haswell', 'r')
-	elif chipset.is_broadwell(devid):
-		args.profile = open('broadwell', 'r')
-	else:
-		print("Autodetect of devid " + hex(devid) + " failed")
+    pci_dev = chipset.intel_get_pci_device()
+    devid = chipset.pcidev_to_devid(pci_dev)
+    if chipset.is_sandybridge(devid):
+        args.profile = open('sandybridge', 'r')
+    elif chipset.is_ivybridge(devid):
+        args.profile = open('ivybridge', 'r')
+    elif chipset.is_valleyview(devid):
+        args.profile = open('valleyview', 'r')
+    elif chipset.is_haswell(devid):
+        args.profile = open('haswell', 'r')
+    elif chipset.is_broadwell(devid):
+        args.profile = open('broadwell', 'r')
+    else:
+        print("Autodetect of devid " + hex(devid) + " failed")
 
 if args.profile == None:
-	sys.exit()
+    sys.exit()
 
 for extra in args.profile:
-	extra_file = open(extra.rstrip(), 'r')
-	parse_file(extra_file)
+    extra_file = open(extra.rstrip(), 'r')
+    parse_file(extra_file)
