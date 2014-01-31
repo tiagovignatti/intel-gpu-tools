bdb08f981c5ebed0ff1f0067f1aa085636b9ac7b
Rafael Barbalho <rafael.barbalho@intel.com>
2014-01-31 14:57:39 +0000

    android: Add the generation of vesion.h & config.h to lib
    The lib directory is used by all the tests & tools so it should really be
    the place where the generate files are created.
    
    Signed-off-by: Rafael Barbalho <rafael.barbalho@intel.com>
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- lib/Android.mk
+++ lib/Android.mk
@@ -1,5 +1,34 @@
 LOCAL_PATH := $(call my-dir)
 
+GPU_TOOLS_PATH := $(LOCAL_PATH)/..
+
+.PHONY: version.h.tmp
+
+$(GPU_TOOLS_PATH)/version.h.tmp:
+	@touch $@
+	@if test -d ../.git; then \
+		if which git > /dev/null; then git log -n 1 --oneline | \
+		        sed 's/^\([^ ]*\) .*/#define IGT_GIT_SHA1 "g\1"/' \
+		        >> $@ ; \
+		fi \
+	else \
+		echo '#define IGT_GIT_SHA1 "NOT-GIT"' >> $@ ; \
+	fi
+
+$(GPU_TOOLS_PATH)/version.h: $(GPU_TOOLS_PATH)/version.h.tmp
+	@echo "updating version.h"
+	@if ! cmp -s $(GPU_TOOLS_PATH)/version.h.tmp $(GPU_TOOLS_PATH)/version.h; then \
+		mv $(GPU_TOOLS_PATH)/version.h.tmp $(GPU_TOOLS_PATH)/version.h ; \
+	else \
+		rm $(GPU_TOOLS_PATH)/version.h.tmp ; \
+	fi
+
+# FIXME: autogenerate this info #
+$(GPU_TOOLS_PATH)/config.h:
+	@echo "updating config.h"
+	@echo '#define PACKAGE_VERSION "1.5"' >> $@ ; \
+	echo '#define TARGET_CPU_PLATFORM "android-ia"' >> $@ ;
+
 include $(LOCAL_PATH)/Makefile.sources
 
 skip_lib_list := \
@@ -11,6 +40,10 @@ lib_list := $(filter-out $(skip_lib_list),$(libintel_tools_la_SOURCES))
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(lib_list)
+
+LOCAL_GENERATED_SOURCES :=       \
+	$(GPU_TOOLS_PATH)/version.h  \
+	$(GPU_TOOLS_PATH)/config.h
 
 LOCAL_C_INCLUDES +=              \
 	$(LOCAL_PATH)/..
