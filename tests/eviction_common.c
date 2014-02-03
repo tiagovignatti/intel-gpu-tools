e1dea7e2e190307056a05a2945e59b59fe5b6fc0
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2014-02-03 10:59:40 +0000

    tests/gem_evict_everything: Factor out eviction logic
    In preparation for userptr test we move the eviction logic
    into a common file so it can be used from both test cases.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

---
+++ tests/eviction_common.c
@@ -0,0 +1,235 @@
+/*
+ * Copyright © 2007, 2011, 2013, 2014 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Eric Anholt <eric@anholt.net>
+ *    Daniel Vetter <daniel.vetter@ffwll.ch>
+ *    Tvrtko Ursulin <tvrtko.ursulin@intel.com>
+ *
+ */
+
+#include <stdlib.h>
+
+#include "drmtest.h"
+
+struct igt_eviction_test_ops
+{
+	uint32_t (*create)(int fd, int size);
+	void	 (*close)(int fd, uint32_t bo);
+	void	 (*copy)(int fd, uint32_t dst, uint32_t src,
+			 uint32_t *all_bo, int nr_bos, int error);
+	void	 (*clear)(int fd, uint32_t bo, int size);
+};
+
+#define FORKING_EVICTIONS_INTERRUPTIBLE	  (1 << 0)
+#define FORKING_EVICTIONS_SWAPPING	  (1 << 1)
+#define FORKING_EVICTIONS_DUP_DRMFD	  (1 << 2)
+#define FORKING_EVICTIONS_MEMORY_PRESSURE (1 << 3)
+#define ALL_FORKING_EVICTIONS	(FORKING_EVICTIONS_INTERRUPTIBLE | \
+				 FORKING_EVICTIONS_SWAPPING | \
+				 FORKING_EVICTIONS_DUP_DRMFD | \
+				 FORKING_EVICTIONS_MEMORY_PRESSURE)
+
+static void exchange_uint32_t(void *array, unsigned i, unsigned j)
+{
+	uint32_t *i_arr = array;
+	uint32_t i_tmp;
+
+	i_tmp = i_arr[i];
+	i_arr[i] = i_arr[j];
+	i_arr[j] = i_tmp;
+}
+
+static int minor_evictions(int fd, struct igt_eviction_test_ops *ops,
+				int surface_size, int nr_surfaces)
+{
+	uint32_t *bo, *sel;
+	int n, m, pass, fail;
+
+	igt_require((uint64_t)nr_surfaces * surface_size / (1024 * 1024)
+			< intel_get_total_ram_mb() * 9 / 10);
+
+	bo = malloc(3*nr_surfaces*sizeof(*bo));
+	igt_assert(bo);
+
+	for (n = 0; n < 2*nr_surfaces; n++)
+		bo[n] = ops->create(fd, surface_size);
+
+	sel = bo + n;
+	for (fail = 0, m = 0; fail < 10; fail++) {
+		for (pass = 0; pass < 100; pass++) {
+			for (n = 0; n < nr_surfaces; n++, m += 7)
+				sel[n] = bo[m%(2*nr_surfaces)];
+			ops->copy(fd, sel[0], sel[1], sel, nr_surfaces, 0);
+		}
+		ops->copy(fd, bo[0], bo[0], bo, 2*nr_surfaces, ENOSPC);
+	}
+
+	for (n = 0; n < 2*nr_surfaces; n++)
+		ops->close(fd, bo[n]);
+	free(bo);
+
+	return 0;
+}
+
+static int major_evictions(int fd, struct igt_eviction_test_ops *ops,
+				int surface_size, int nr_surfaces)
+{
+	int n, m, loop;
+	uint32_t *bo;
+
+	igt_require((uint64_t)nr_surfaces * surface_size / (1024 * 1024)
+			< intel_get_total_ram_mb() * 9 / 10);
+
+	bo = malloc(nr_surfaces*sizeof(*bo));
+	igt_assert(bo);
+
+	for (n = 0; n < nr_surfaces; n++)
+		bo[n] = ops->create(fd, surface_size);
+
+	for (loop = 0, m = 0; loop < 100; loop++, m += 17) {
+		n = m % nr_surfaces;
+		ops->copy(fd, bo[n], bo[n], &bo[n], 1, 0);
+	}
+
+	for (n = 0; n < nr_surfaces; n++)
+		ops->close(fd, bo[n]);
+	free(bo);
+
+	return 0;
+}
+
+static int swapping_evictions(int fd, struct igt_eviction_test_ops *ops,
+				int surface_size,
+				int working_surfaces,
+				int trash_surfaces)
+{
+	uint32_t *bo;
+	int i, n, pass;
+
+	igt_require((uint64_t)working_surfaces * surface_size / (1024 * 1024)
+			< intel_get_total_ram_mb() * 9 / 10);
+
+	if (trash_surfaces < working_surfaces)
+		trash_surfaces = working_surfaces;
+
+	bo = malloc(trash_surfaces*sizeof(*bo));
+	igt_assert(bo);
+
+	for (n = 0; n < trash_surfaces; n++)
+		bo[n] = ops->create(fd, surface_size);
+
+	for (i = 0; i < trash_surfaces/32; i++) {
+		igt_permute_array(bo, trash_surfaces, exchange_uint32_t);
+
+		for (pass = 0; pass < 100; pass++) {
+			ops->copy(fd, bo[0], bo[1], bo, working_surfaces, 0);
+		}
+	}
+
+	for (n = 0; n < trash_surfaces; n++)
+		ops->close(fd, bo[n]);
+	free(bo);
+
+	return 0;
+}
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+static int forking_evictions(int fd, struct igt_eviction_test_ops *ops,
+				int surface_size, int working_surfaces,
+				int trash_surfaces, unsigned flags)
+{
+	uint32_t *bo;
+	int n, pass, l;
+	int num_threads = sysconf(_SC_NPROCESSORS_ONLN);
+	int bo_count;
+
+	igt_require((uint64_t)working_surfaces * surface_size / (1024 * 1024)
+			< intel_get_total_ram_mb() * 9 / 10);
+
+	if (flags & FORKING_EVICTIONS_SWAPPING) {
+		igt_require(intel_get_total_ram_mb() / 4
+				< intel_get_total_swap_mb());
+		bo_count = trash_surfaces;
+
+		if (bo_count < working_surfaces)
+			bo_count = working_surfaces;
+	} else
+		bo_count = working_surfaces;
+
+	bo = malloc(bo_count*sizeof(*bo));
+	igt_assert(bo);
+
+	for (n = 0; n < bo_count; n++)
+		bo[n] = ops->create(fd, surface_size);
+
+	igt_fork(i, min(num_threads * 4, 12)) {
+		int realfd = fd;
+		int num_passes = flags & FORKING_EVICTIONS_SWAPPING ? 10 : 100;
+
+		/* Every fork should have a different permutation! */
+		srand(i * 63);
+
+		if (flags & FORKING_EVICTIONS_INTERRUPTIBLE)
+			igt_fork_signal_helper();
+
+		igt_permute_array(bo, bo_count, exchange_uint32_t);
+
+		if (flags & FORKING_EVICTIONS_DUP_DRMFD) {
+			realfd = drm_open_any();
+
+			/* We can overwrite the bo array since we're forked. */
+			for (l = 0; l < bo_count; l++) {
+				uint32_t flink;
+
+				flink = gem_flink(fd, bo[l]);
+				bo[l] = gem_open(realfd, flink);
+			}
+		}
+
+		for (pass = 0; pass < num_passes; pass++) {
+			ops->copy(realfd, bo[0], bo[1], bo, working_surfaces, 0);
+
+			for (l = 0; l < working_surfaces &&
+			  (flags & FORKING_EVICTIONS_MEMORY_PRESSURE);
+			    l++) {
+				ops->clear(realfd, bo[l], surface_size);
+			}
+		}
+
+		if (flags & FORKING_EVICTIONS_INTERRUPTIBLE)
+			igt_stop_signal_helper();
+
+		/* drmfd closing will take care of additional bo refs */
+		if (flags & FORKING_EVICTIONS_DUP_DRMFD)
+			close(realfd);
+	}
+
+	igt_waitchildren();
+
+	for (n = 0; n < bo_count; n++)
+		ops->close(fd, bo[n]);
+	free(bo);
+
+	return 0;
+}
--- tests/gem_evict_everything.c
+++ tests/gem_evict_everything.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011,2012 Intel Corporation
+ * Copyright © 2011,2012,2014 Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -44,6 +44,8 @@
 #include "drm.h"
 #include "i915_drm.h"
 #include "drmtest.h"
+
+#include "eviction_common.c"
 
 #define HEIGHT 256
 #define WIDTH 1024
@@ -125,183 +127,51 @@ copy(int fd, uint32_t dst, uint32_t src, uint32_t *all_bo, int n_bo, int error)
 	free(obj);
 }
 
-static void exchange_uint32_t(void *array, unsigned i, unsigned j)
+static void clear(int fd, uint32_t handle, int size)
 {
-	uint32_t *i_arr = array;
-	uint32_t i_tmp;
+	void *base = gem_mmap__cpu(fd, handle, size, PROT_READ | PROT_WRITE);
 
-	i_tmp = i_arr[i];
-	i_arr[i] = i_arr[j];
-	i_arr[j] = i_tmp;
+	igt_assert(base != NULL);
+	memset(base, 0, size);
+	munmap(base, size);
 }
 
-#define min(a, b) ((a) < (b) ? (a) : (b))
+static struct igt_eviction_test_ops fault_ops = {
+	.create = gem_create,
+	.close = gem_close,
+	.copy = copy,
+	.clear = clear,
+};
 
-#define INTERRUPTIBLE	(1 << 0)
-#define SWAPPING	(1 << 1)
-#define DUP_DRMFD	(1 << 2)
-#define MEMORY_PRESSURE	(1 << 3)
-#define ALL_FLAGS	(INTERRUPTIBLE | SWAPPING | DUP_DRMFD | MEMORY_PRESSURE)
-
-static void forked_evictions(int fd, int size, int count,
-			     unsigned flags)
+static void test_forking_evictions(int fd, int size, int count,
+					unsigned flags)
 {
-	uint32_t *bo;
-	int n, pass, l;
-	int num_threads = sysconf(_SC_NPROCESSORS_ONLN);
-	int bo_count;
+	int trash_count;
 
-	igt_require((uint64_t)count * size / (1024 * 1024) < intel_get_total_ram_mb() * 9 / 10);
+	trash_count = intel_get_total_ram_mb() * 11 / 10;
 
-	if (flags & SWAPPING) {
-		igt_require(intel_get_total_ram_mb() / 4 < intel_get_total_swap_mb());
-		bo_count = intel_get_total_ram_mb() * 11 / 10;
-
-		if (bo_count < count)
-			bo_count = count;
-	} else
-		bo_count = count;
-
-	bo = malloc(bo_count*sizeof(*bo));
-	igt_assert(bo);
-
-	for (n = 0; n < bo_count; n++)
-		bo[n] = gem_create(fd, size);
-
-	igt_fork(i, min(count, min(num_threads * 5, 12))) {
-		int realfd = fd;
-		int num_passes = flags & SWAPPING ? 10 : 100;
-
-		/* Every fork should have a different permutation! */
-		srand(i * 63);
-
-		if (flags & INTERRUPTIBLE)
-			igt_fork_signal_helper();
-
-		igt_permute_array(bo, bo_count, exchange_uint32_t);
-
-		if (flags & DUP_DRMFD) {
-			realfd = drm_open_any();
-
-			/* We can overwrite the bo array since we're forked. */
-			for (l = 0; l < count; l++) {
-				uint32_t flink;
-
-				flink = gem_flink(fd, bo[l]);
-				bo[l] = gem_open(realfd, flink);
-			}
-
-		}
-
-		for (pass = 0; pass < num_passes; pass++) {
-			copy(realfd, bo[0], bo[1], bo, count, 0);
-
-			for (l = 0; l < count && (flags & MEMORY_PRESSURE); l++) {
-				uint32_t *base = gem_mmap__cpu(realfd, bo[l],
-							       size,
-							       PROT_READ | PROT_WRITE);
-				memset(base, 0, size);
-				munmap(base, size);
-			}
-		}
-
-		if (flags & INTERRUPTIBLE)
-			igt_stop_signal_helper();
-
-		/* drmfd closing will take care of additional bo refs */
-		if (flags & DUP_DRMFD)
-			close(realfd);
-	}
-
-	igt_waitchildren();
-
-	for (n = 0; n < bo_count; n++)
-		gem_close(fd, bo[n]);
-	free(bo);
+	forking_evictions(fd, &fault_ops, size, count, trash_count, flags);
 }
 
-static void swapping_evictions(int fd, int size, int count)
+static void test_swapping_evictions(int fd, int size, int count)
 {
-	uint32_t *bo;
-	int i, n, pass;
-	int bo_count;
-
-	igt_require((uint64_t)count * size / (1024 * 1024) < intel_get_total_ram_mb() * 9 / 10);
+	int trash_count;
 
 	igt_require(intel_get_total_ram_mb() / 4 < intel_get_total_swap_mb());
-	bo_count = intel_get_total_ram_mb() * 11 / 10;
 
-	if (bo_count < count)
-		bo_count = count;
+	trash_count = intel_get_total_ram_mb() * 11 / 10;
 
-	bo = malloc(bo_count*sizeof(*bo));
-	igt_assert(bo);
-
-	for (n = 0; n < bo_count; n++)
-		bo[n] = gem_create(fd, size);
-
-	for (i = 0; i < bo_count/32; i++) {
-		igt_permute_array(bo, bo_count, exchange_uint32_t);
-
-		for (pass = 0; pass < 100; pass++) {
-			copy(fd, bo[0], bo[1], bo, count, 0);
-		}
-	}
-
-	for (n = 0; n < bo_count; n++)
-		gem_close(fd, bo[n]);
-	free(bo);
+	swapping_evictions(fd, &fault_ops, size, count, trash_count);
 }
 
-static void minor_evictions(int fd, int size, int count)
+static void test_minor_evictions(int fd, int size, int count)
 {
-	uint32_t *bo, *sel;
-	int n, m, pass, fail;
-
-	igt_require((uint64_t)count * size / (1024 * 1024) < intel_get_total_ram_mb() * 9 / 10);
-
-	bo = malloc(3*count*sizeof(*bo));
-	igt_assert(bo);
-
-	for (n = 0; n < 2*count; n++)
-		bo[n] = gem_create(fd, size);
-
-	sel = bo + n;
-	for (fail = 0, m = 0; fail < 10; fail++) {
-		for (pass = 0; pass < 100; pass++) {
-			for (n = 0; n < count; n++, m += 7)
-				sel[n] = bo[m%(2*count)];
-			copy(fd, sel[0], sel[1], sel, count, 0);
-		}
-		copy(fd, bo[0], bo[0], bo, 2*count, ENOSPC);
-	}
-
-	for (n = 0; n < 2*count; n++)
-		gem_close(fd, bo[n]);
-	free(bo);
+	minor_evictions(fd, &fault_ops, size, count);
 }
 
-static void major_evictions(int fd, int size, int count)
+static void test_major_evictions(int fd, int size, int count)
 {
-	int n, m, loop;
-	uint32_t *bo;
-
-	igt_require((uint64_t)count * size / (1024 * 1024) < intel_get_total_ram_mb() * 9 / 10);
-
-	bo = malloc(count*sizeof(*bo));
-	igt_assert(bo);
-
-	for (n = 0; n < count; n++)
-		bo[n] = gem_create(fd, size);
-
-	for (loop = 0, m = 0; loop < 100; loop++, m += 17) {
-		n = m % count;
-		copy(fd, bo[n], bo[n], &bo[n], 1, 0);
-	}
-
-	for (n = 0; n < count; n++)
-		gem_close(fd, bo[n]);
-	free(bo);
+	major_evictions(fd, &fault_ops, size, count);
 }
 
 igt_main
@@ -319,26 +189,28 @@ igt_main
 		count = 3*gem_aperture_size(fd) / size / 4;
 	}
 
-	for (unsigned flags = 0; flags < ALL_FLAGS + 1; flags++) {
+	for (unsigned flags = 0; flags < ALL_FORKING_EVICTIONS + 1; flags++) {
 		igt_subtest_f("forked%s%s%s-%s",
-			      flags & SWAPPING ? "-swapping" : "",
-			      flags & DUP_DRMFD ? "-multifd" : "",
-			      flags & MEMORY_PRESSURE ? "-mempressure" : "",
-			      flags & INTERRUPTIBLE ? "interruptible" : "normal") {
-			forked_evictions(fd, size, count, flags);
+		    flags & FORKING_EVICTIONS_SWAPPING ? "-swapping" : "",
+		    flags & FORKING_EVICTIONS_DUP_DRMFD ? "-multifd" : "",
+		    flags & FORKING_EVICTIONS_MEMORY_PRESSURE ?
+				"-mempressure" : "",
+		    flags & FORKING_EVICTIONS_INTERRUPTIBLE ?
+				"interruptible" : "normal") {
+			test_forking_evictions(fd, size, count, flags);
 		}
 	}
 
 	igt_subtest("swapping-normal")
-		swapping_evictions(fd, size, count);
+		test_swapping_evictions(fd, size, count);
 
 	igt_subtest("minor-normal")
-		minor_evictions(fd, size, count);
+		test_minor_evictions(fd, size, count);
 
 	igt_subtest("major-normal") {
 		size = 3*gem_aperture_size(fd) / 4;
 		count = 4;
-		major_evictions(fd, size, count);
+		test_major_evictions(fd, size, count);
 	}
 
 	igt_fixture {
@@ -349,15 +221,15 @@ igt_main
 	igt_fork_signal_helper();
 
 	igt_subtest("swapping-interruptible")
-		swapping_evictions(fd, size, count);
+		test_swapping_evictions(fd, size, count);
 
 	igt_subtest("minor-interruptible")
-		minor_evictions(fd, size, count);
+		test_minor_evictions(fd, size, count);
 
 	igt_subtest("major-interruptible") {
 		size = 3*gem_aperture_size(fd) / 4;
 		count = 4;
-		major_evictions(fd, size, count);
+		test_major_evictions(fd, size, count);
 	}
 
 	igt_stop_signal_helper();
