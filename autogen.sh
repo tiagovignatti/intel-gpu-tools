14ea2a04ebfa287e33b69a3bc7e47f02ba93c9ed
Thomas Wood <thomas.wood@intel.com>
2014-03-11 16:04:28 +0000

    Add API documentation support
    Add optional support for building API documentation using gtk-doc.
    
    Signed-off-by: Thomas Wood <thomas.wood@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,13 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
+if ! type gtkdocize > /dev/null 2>&1; then
+        echo "EXTRA_DIST =" > gtk-doc.make
+        echo "CLEANFILES =" >> gtk-doc.make
+else
+        gtkdocize || exit $?
+fi
+
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
