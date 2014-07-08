9cf7e8d35fb331626a03992f68e9856eafe3997d
Damien Lespiau <damien.lespiau@intel.com>
2014-07-08 12:08:20 +0100

    kms_rotation_crc: Update the copyright to have this year as well
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- tests/kms_rotation_crc.c
+++ tests/kms_rotation_crc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Intel Corporation
+ * Copyright © 2013,2014 Intel Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
