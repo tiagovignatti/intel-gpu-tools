1658edceee85bd5f57e8dd39075f1d127260d5b6
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-11-28 10:03:38 +0200

    lib: Add swap() macro
    swap() will swap its two arguments while keeping the required
    tmp variable hidden. Makes for neater code.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- lib/igt_aux.h
+++ lib/igt_aux.h
@@ -90,4 +90,10 @@ void intel_require_memory(uint32_t count, uint32_t size, unsigned mode);
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) > (b) ? (a) : (b))
 
+#define swap(a, b) do {		\
+	typeof(a) _tmp = (a);	\
+	(a) = (b);		\
+	(b) = _tmp;		\
+} while (0)
+
 #endif /* IGT_AUX_H */
