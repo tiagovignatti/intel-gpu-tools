7bd4f918c46135762e14c359a5b3c6340364082c
Chris Wilson <chris@chris-wilson.co.uk>
2016-04-14 17:03:07 +0100

    igt/gem_exec_nop: Explain the parallel execution assertion
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- tests/gem_exec_nop.c
+++ tests/gem_exec_nop.c
@@ -187,7 +187,9 @@ static void all(int fd, uint32_t handle, int timeout)
 	 * is frozen. So in general we cannot go faster than the slowest
 	 * engine, but we should equally not go any slower.
 	 */
-	igt_assert(time < max + min); /* ensure parallel execution */
+	igt_assert_f(time < max + 10*min/9, /* ensure parallel execution */
+		     "Average time (%.3fus) exceeds expecation for parallel execution (min %.3fus, max %3.fus; limit set at %.3fus)\n",
+		     time, min, max, max + 10*min/9);
 }
 
 igt_main
