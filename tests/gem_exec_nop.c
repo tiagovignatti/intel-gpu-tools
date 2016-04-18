77b8a5bfd386b8cec1b5df55f6d28a130a0356f9
Chris Wilson <chris@chris-wilson.co.uk>
2016-04-18 10:26:05 +0100

    igt/gem_exec_nop: Fix time units for assertion printf
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

--- tests/gem_exec_nop.c
+++ tests/gem_exec_nop.c
@@ -189,7 +189,7 @@ static void all(int fd, uint32_t handle, int timeout)
 	 */
 	igt_assert_f(time < max + 10*min/9, /* ensure parallel execution */
 		     "Average time (%.3fus) exceeds expecation for parallel execution (min %.3fus, max %.3fus; limit set at %.3fus)\n",
-		     time, min, max, max + 10*min/9);
+		     1e6*time, 1e6*min, 1e6*max, 1e6*(max + 10*min/9));
 }
 
 igt_main
